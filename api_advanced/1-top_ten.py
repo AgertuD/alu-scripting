b3947dbbded7bf80d4b4e5de2e4b51a86a588eec
root <root@DESKTOP-59TERCB.localdomain>
2025-02-21 23:17:29 +0200

    number 1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
-"""
-Module to fetch and display the top ten hot posts of a given subreddit using Reddit API.
-"""
-
+""" top_ten.py """
 import requests
 
-def top_ten(subreddit):
-    """Queries the Reddit API and prints the titles of the first 10 hot posts"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'MyRedditScript/1.0'}
-    params = {'limit': 10}
-    
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-    
-    if response.status_code == 200:
-        data = response.json()
-        posts = data.get('data', {}).get('children', [])
-        
-        for post in posts:
-            print(post['data']['title'])
-    else:
-        print(None)
 
+def top_ten(subreddit):
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print(None)
+        return
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
